d94fc5e5de2ec1add4926ad8051ce5e3ae7a3567
Tony Jian <tonytonyjan@gmail.com>
2012-10-31 15:03:24 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tjexam::Application.config.secret_token = '5c4fe28a074950520dafbed594a34013db295de2b473a82a4e5ed24133f5fecb12ad285717c6c12ddf9ca335414e3b5f833cc06048147f1e6f5fee23f862f0a1'
