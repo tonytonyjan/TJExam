413b16f512d3810a0ae472426d793f58b3150226
Tony Jian <tonytonyjan@gmail.com>
2012-11-19 17:00:50 +0800

    add parser library

---
+++ lib/importing_tools/gen.rb
@@ -0,0 +1,25 @@
+require 'tmpdir'
+require './parser'
+
+module TJExam
+
+  def self.gen doc_filepath
+    ary = nil
+    Dir.mktmpdir{|dir|
+      `unoconv -o #{dir} -f html #{doc_filepath}`
+      unless $?.success?
+        $stderr.puts $?
+        break
+      end
+      html_file_name = File::join(dir, File::basename(doc_filepath).sub(/[^.]+$/, 'html'))
+      File::open(html_file_name){|f| 
+        ary = TJExam::parse(f)
+      }
+    }
+    ary
+  end
+end
+
+if $0 == __FILE__
+  p TJExam::gen "/home/tonytonyjan/codes/tmp/90math-1_format.doc"
+end
---
+++ lib/importing_tools/parser.rb
@@ -0,0 +1,27 @@
+require 'nokogiri'
+require 'pp'
+
+module TJExam
+  # returns an array of content
+  def self.parse file
+    doc = Nokogiri::HTML(file)
+    doc.css('script, link').each {|node| node.remove}
+    # Convert image tag to Markdown format
+    # doc.css('body img').each{|node| node.replace("![alt](#{node['src']})")}
+    doc.css('body *').each{|node|
+      unless %w(img table tr td thead tbody).include?(node.name)
+        # Strip tag
+        node.swap(node.children)
+      else
+        # Keep image source
+        node.attributes.each_value{|attr| attr.remove unless attr.name == "src"}
+      end
+    }
+    separator = "==@@T_BEGIN@@=="
+    doc.css('body').inner_html.gsub(/(\n\t|\n|\t|\s)+/, '\1').split(separator)
+  end
+end
+
+if $0 == __FILE__
+  TJExam::parse(File::open("/home/tonytonyjan/codes/tmp/90math-1_format/90math-1_format.html"))
+end
