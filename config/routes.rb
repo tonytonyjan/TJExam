9f271ec02991ba3e7d66c7d4358aafc048dd4794
Tony Jian <tonytonyjan@gmail.com>
2012-12-13 20:52:21 +0800

    remove action about and contact

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,6 @@ Tjexam::Application.routes.draw do
   end
 
   get "home/index"
-  get "home/about"
-  get "home/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
